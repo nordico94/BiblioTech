c050bf710bc7ae56dac30656bb3ae2d5bd1a4782
Juan Ignacio Albornoz <albornoz.juan.main@gmail.com>
2026-09-13 22:04:12 -0300

    Agregado de libros (15)

---
+++ js/data.js
@@ -0,0 +1,128 @@
+const libros = [
+    {
+        id: 1,
+        titulo: "Dragon Ball tomo 1",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1984,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 2,
+        titulo: "Dragon Ball tomo 2",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1984,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 3,
+        titulo: "Dragon Ball tomo 3",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1984,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 4,
+        titulo: "Dragon Ball Z tomo 1",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1989,
+        disponible: true,
+        favorito: false
+    },
+        {
+        id: 5,
+        titulo: "Dragon Ball Z tomo 2",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1989,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 6,
+        titulo: "Dragon Ball Z tomo 3",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1989,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 7,
+        titulo: "Dragon Ball Z tomo 4",
+        autor: "Akira Toriyama",
+        genero: "Manga",
+        año: 1989,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 8,
+        titulo: "Harry Potter y la piedra filosofal",
+        autor: "J.K. Rowling",
+        genero: "Fantasía",
+        año: 1997,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 9,
+        titulo: "Harry Potter y la cámara secreta",
+        autor: "J.K. Rowling",
+        genero: "Fantasía",
+        año: 1998,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 10,
+        titulo: "Harry Potter y el prisionero de Azkaban",
+        autor: "J.K. Rowling",
+        genero: "Fantasía",
+        año: 1999,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 11,
+        titulo: "Harry Potter y el cáliz de fuego",
+        autor: "J.K. Rowling",
+        genero: "Fantasía",
+        año: 2000,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 12,
+        titulo: "El señor de los anillos: La comunidad del anillo",
+        autor: "J.R.R. Tolkein",
+        genero: "Fantasía",
+        año: 1954,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 13,
+        titulo: "El señor de los anillos: Las dos torres",
+        autor: "J.R.R. Tolkein",
+        genero: "Fantasía",
+        año: 1954,
+        disponible: true,
+        favorito: false
+    },
+    {
+        id: 14,
+        titulo: "El señor de los anillos: El retorno del rey",
+        autor: "J.R.R. Tolkein",
+        genero: "Fantasía",
+        año: 1955,
+        disponible: true,
+        favorito: false
+    }
+];
